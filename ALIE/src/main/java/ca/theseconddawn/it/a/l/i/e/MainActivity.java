613387bea5c8083b3498d693edf8faa17c2e737b
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-09-23 00:33:44 -0400

    Initial Commit for Project A.L.I.E By: Paramand Mohabir

---
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+Paramand Mohabir N01421732
+CENG322 Section 0NC
+Software Project
+*/
+
+package ca.theseconddawn.it.a.l.i.e;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
