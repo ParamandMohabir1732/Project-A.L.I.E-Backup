f27c4bc4c5c6d975b131a8c84f3ea4a5df29bf95
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-09-24 23:15:01 -0400

    I've implemented the Navigation View and the ToolBar in MainActivity.java. I also added a ToolBar theme in themes.xml to make the ToolBar appear in the activity level and still show up in the A.L.I.E App. I also modified the Manifest file to implement the ToolBar Theme.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/MainActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/MainActivity.java
@@ -21,19 +21,47 @@ Software Project
 
 package ca.theseconddawn.it.a.l.i.e;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    public Toolbar toolbar;
+    public NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.TheSecondDawnToolBar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.TheSecondDawnDrawerLayout);
+        navigationView = findViewById(R.id.TheSecondDawnNavigationView);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openNavigationDrawer, R.string.closeNavigationDrawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
     public void onBackPressed () {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle(R.string.builderTitle);
         builder.setMessage(R.string.builderMessage);
