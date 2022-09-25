e0f6bc6252d2e2a379e65206d6e538d3d4bdf54c
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-09-25 02:17:59 -0400

    I've created the Toolbar menu to implement into the Navigation Drawer and I also implemented a settings functionality into the Toolbar. I also added some extra colors in colors.xml. In my MainActivity.java, I added Navigation Viewer Functionality with function prototypes to enable the Navigation Viewer to function. I also created a Home Fragment and implemented it into the Navigation Viewer that will be used in later time for our Software Project.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/MainActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/MainActivity.java
@@ -17,10 +17,21 @@ Software Project
 Paolo Brancato N01434080
 CENG322 Section ONC
 Software Project
- */
+*/
 
 package ca.theseconddawn.it.a.l.i.e;
 
+import static ca.theseconddawn.it.a.l.i.e.R.string.toastMessage1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,11 +39,9 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     public Toolbar toolbar;
@@ -48,10 +57,43 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.TheSecondDawnDrawerLayout);
         navigationView = findViewById(R.id.TheSecondDawnNavigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openNavigationDrawer, R.string.closeNavigationDrawer);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.TheSecondDawnFragmentContainer, new HomeFrag()).commit();
+        navigationView.setCheckedItem(R.id.TheSecondDawnNavHome);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.TheSecondDawnNavHome) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.TheSecondDawnFragmentContainer, new HomeFrag()).commit();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.TheSecondDawnToolBarSettings) {
+            Toast.makeText(this, toastMessage1, Toast.LENGTH_LONG).show();
+            Intent settings = new Intent(Settings.ACTION_SETTINGS);
+            startActivity(settings);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onBackPressed () {
