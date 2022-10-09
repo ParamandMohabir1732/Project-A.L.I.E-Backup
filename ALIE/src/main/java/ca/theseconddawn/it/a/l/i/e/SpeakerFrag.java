b05c091f7d39c22fcf7b79cd4f25fce1bf2c5190
DavePatel5129 <Dave.p07@outlook.com>
2022-10-09 03:57:14 -0400

    Created fragment java class named SpeakerFrag

---
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SpeakerFrag.java
@@ -0,0 +1,24 @@
+package ca.theseconddawn.it.a.l.i.e;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SpeakerFrag extends Fragment {
+
+
+    public SpeakerFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_speaker, container, false);
+    }
+}
