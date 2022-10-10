de225d6f441f3cc304589f4d22233bb6474a996c
PaoloBrancato4080 <paobono@live.com>
2022-10-10 19:04:54 -0400

    I have created a new fragment called LEDFrag and also changed the background color to green. I also created the LED vector image that will be used in the navigation drawer later.

---
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/LEDFrag.java
@@ -0,0 +1,39 @@
+/*
+Paramand Mohabir N01421732
+CENG322 Section 0NC
+Software Project
+*/
+/*
+Vladislav Vassilyev N01436627
+CENG322 Section 0NB
+Software Project
+*/
+/*
+Dave Patel N01465129
+CENG322 Section 0NA
+Software Project
+*/
+/*
+Paolo Brancato N01434080
+CENG322 Section ONC
+Software Project
+*/
+
+package ca.theseconddawn.it.a.l.i.e;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LEDFrag extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_l_e_d, container, false);
+        return view;
+    }
+}
