acde7384acd10ba5e1c5aa8e8bfadebee756b065
DavePatel5129 <Dave.p07@outlook.com>
2022-11-10 04:08:06 -0500

    implemented seekBar to control volume in SpeakerFrag.java

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SpeakerFrag.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SpeakerFrag.java
@@ -21,18 +21,52 @@ Software Project
 
 package ca.theseconddawn.it.a.l.i.e;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 
 import androidx.fragment.app.Fragment;
 
 public class SpeakerFrag extends Fragment {
 
+    AudioManager audioManager;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_speaker, container, false);
+
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+
+        SeekBar seekBar = (SeekBar) view.findViewById(R.id.volumeSeekbar);
+        seekBar.setMax(maxVolume);
+        seekBar.setProgress(currentVolume);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i ,0 );
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         return view;
     }
 }
