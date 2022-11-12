cb89f07da94627d9214267391cbf0a9d7db9a857
DavePatel5129 <Dave.p07@outlook.com>
2022-11-12 05:00:49 -0500

    implemented onClickListener on button which opens dialer (HelpActivity.java)

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/HelpActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/HelpActivity.java
@@ -2,7 +2,10 @@ package ca.theseconddawn.it.a.l.i.e;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
@@ -12,6 +15,18 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        Button btn = (Button) findViewById(R.id.appCompatButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:18009981498"));
+                startActivity(intent);
+            }
+        });
 
-    }
+
+
+
+}
 }
