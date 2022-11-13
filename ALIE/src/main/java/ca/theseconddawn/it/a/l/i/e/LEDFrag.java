594382e9ba28c4b2118c21c1dcc40daea3c3e08f
PaoloBrancato4080 <paobono@live.com>
2022-11-13 00:48:22 -0500

    implemented run time request functionality on LCD screen and added button on LED screen for the request

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/LEDFrag.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/LEDFrag.java
@@ -21,20 +21,26 @@ Software Project
 
 package ca.theseconddawn.it.a.l.i.e;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class LEDFrag extends Fragment {
+    private final int NETWORK_PERMISSION_CODE = 1;
     ConstraintLayout LEDLayout;
     int LEDDefaultColor;
     Button LEDButton;
@@ -43,12 +49,43 @@ public class LEDFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_l_e_d, container, false);
 
-
-        LEDLayout = (ConstraintLayout) view.findViewById(R.id.Layout);
+        LEDLayout = view.findViewById(R.id.Layout);
         LEDButton = view.findViewById(R.id.button);
         LEDDefaultColor = ContextCompat.getColor(getActivity(), com.google.android.material.R.color.design_default_color_on_primary);
         LEDButton.setOnClickListener(view1 -> openColorPicker());
+
+        Button buttonRequest = view.findViewById(R.id.permissionsBtn);
+        buttonRequest.setOnClickListener(view12 -> {
+            if (ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.BLUETOOTH_SCAN) == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), "you have already granted this permission! ", Toast.LENGTH_SHORT).show();
+            } else {
+                requestInternetPermission();
+            }
+        });
         return view;
+    }
+
+
+    private void requestInternetPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.BLUETOOTH_SCAN)) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("A.L.I.E");
+            builder.setMessage("This permission is needed to allow led control to access your wifi connection");
+            builder.setPositiveButton("ok", (dialogInterface, which) -> ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_WIFI_STATE}, NETWORK_PERMISSION_CODE));
+            builder.setNegativeButton("Cancel", (dialogInterface, which) -> dialogInterface.dismiss()).create().show();
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.BLUETOOTH_SCAN}, NETWORK_PERMISSION_CODE);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == NETWORK_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getActivity(), "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void openColorPicker() {
