b63c92caff1a09e570e7da4d67f20a325bcfa868
paolo <paobono@live.com>
2022-11-13 23:59:01 -0500

    I created landscape layout of led screen.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/LEDFrag.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/LEDFrag.java
@@ -29,6 +29,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -45,7 +46,7 @@ public class LEDFrag extends Fragment {
 
     private final int ACCESS_LOCATION_CODE = 1;
 
-    private ConstraintLayout LEDLayout;
+    private LinearLayout LEDLayout;
     private int LEDDefaultColor;
     private Button LEDButton, buttonRequest;
     private AmbilWarnaDialog colorPicker;
