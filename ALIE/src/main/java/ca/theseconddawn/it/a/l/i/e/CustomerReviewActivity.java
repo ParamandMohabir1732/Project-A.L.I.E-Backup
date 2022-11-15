2ba5ca4ba5d1d9b1c56e02562926ed2a45368a31
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-11-14 19:36:38 -0500

    Created Landscape for Customer Review Screen.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/CustomerReviewActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/CustomerReviewActivity.java
@@ -24,17 +24,27 @@ package ca.theseconddawn.it.a.l.i.e;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerReviewActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton leftArrow;
+    private TextView deviceModel;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_review);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        deviceModel = findViewById(R.id.TheSecondDawnTextView28);
 
         leftArrow = findViewById(R.id.TheSecondDawnImageButton10);
         leftArrow.setOnClickListener(this);
