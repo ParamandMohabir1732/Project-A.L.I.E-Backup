ff1d1a25d69e6a38a046c4b0cf3d15f9696fb1f2
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-11-25 00:13:13 -0500

    Implemented Functionality to change Screen rotation in Settings and implemented new Seekbar UI design that will be used to change the volume.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SettingsActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SettingsActivity.java
@@ -21,15 +21,21 @@ Software Project
 
 package ca.theseconddawn.it.a.l.i.e;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton leftArrow;
+    private SwitchCompat orientation;
+    private SeekBar volume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,38 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
 
         leftArrow = findViewById(R.id.TheSecondDawnImageButton3);
         leftArrow.setOnClickListener(this);
+
+        volume = findViewById(R.id.TheSecondDawnSeekBar2);
+        volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        orientation = findViewById(R.id.TheSecondDawnSwitch9);
+        orientation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked) {
+                    orientation.setText("Landscape");
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                } else {
+                    orientation.setText("Portrait");
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                }
+            }
+        });
     }
 
     @Override
