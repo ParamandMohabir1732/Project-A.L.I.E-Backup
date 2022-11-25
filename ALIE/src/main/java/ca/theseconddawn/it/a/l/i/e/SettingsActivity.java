998bd6f1a12110c4be2d040b426d23d0d3f87455
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-11-25 00:46:40 -0500

    Implemented Functionality and UI of seekbar to control volume of device in SettingsActivity.java.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SettingsActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SettingsActivity.java
@@ -21,12 +21,15 @@ Software Project
 
 package ca.theseconddawn.it.a.l.i.e;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
@@ -36,20 +39,30 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     private ImageButton leftArrow;
     private SwitchCompat orientation;
     private SeekBar volume;
+    private TextView volumeProgress;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings2);
 
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
         leftArrow = findViewById(R.id.TheSecondDawnImageButton3);
         leftArrow.setOnClickListener(this);
 
+        volumeProgress = findViewById(R.id.TheSecondDawnTextView37);
+        audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+
         volume = findViewById(R.id.TheSecondDawnSeekBar2);
+        volume.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        volume.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
         volume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                volumeProgress.setText("" + progress);
             }
 
             @Override
@@ -64,16 +77,13 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         });
 
         orientation = findViewById(R.id.TheSecondDawnSwitch9);
-        orientation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (isChecked) {
-                    orientation.setText("Landscape");
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                } else {
-                    orientation.setText("Portrait");
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-                }
+        orientation.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            if (isChecked) {
+                orientation.setText("Landscape");
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else {
+                orientation.setText("Portrait");
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
             }
         });
     }
