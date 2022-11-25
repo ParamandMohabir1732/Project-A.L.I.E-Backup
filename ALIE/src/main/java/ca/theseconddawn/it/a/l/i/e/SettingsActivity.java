adae341e8f7e3b67e69862c95329437c6b39de82
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-11-24 19:39:06 -0500

    Implemented new Settings Screen. Beta Stages.

---
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/SettingsActivity.java
@@ -0,0 +1,35 @@
+/*
+Paramand Mohabir N01421732
+CENG322 Section 0NC
+Software Project
+*/
+/*
+Vladislav Vassilyev N01436627
+CENG322 Section 0NB
+Software Project
+*/
+/*
+Dave Patel N01465129
+CENG322 Section 0NA
+Software Project
+*/
+/*
+Paolo Brancato N01434080
+CENG322 Section ONC
+Software Project
+*/
+
+package ca.theseconddawn.it.a.l.i.e;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings2);
+    }
+}
