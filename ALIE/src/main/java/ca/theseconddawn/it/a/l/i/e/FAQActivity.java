98880a810ec8effb90aa3ee7e8910e0da248865a
DavePatel5129 <Dave.p07@outlook.com>
2022-11-27 03:06:57 -0500

    Added imageView with onClickListener in FAQ page

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/FAQActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/FAQActivity.java
@@ -2,13 +2,34 @@ package ca.theseconddawn.it.a.l.i.e;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FAQActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqactivity);
+
+        // initialize imageView
+        // with method findViewById()
+        imageView = findViewById(R.id.imageView4);
+
+        // Apply OnClickListener  to imageView to
+        // switch from one activity to another
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FAQActivity.this, ContactUsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
