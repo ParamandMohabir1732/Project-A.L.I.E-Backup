c737dc7021e22644f78d771ca832663c665758cb
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-11-29 01:11:37 -0500

    Implemented Left Arrow Function Button to go back to Previous Screen,

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/FAQActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/FAQActivity.java
@@ -5,17 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
-public class FAQActivity extends AppCompatActivity {
+public class FAQActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageView;
-
+    private ImageButton leftArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faqactivity);
+
+        leftArrow = findViewById(R.id.TheSecondDawnImageButton12);
+        leftArrow.setOnClickListener(this);
 
         // initialize imageView
         // with method findViewById()
@@ -23,13 +27,17 @@ public class FAQActivity extends AppCompatActivity {
 
         // Apply OnClickListener  to imageView to
         // switch from one activity to another
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FAQActivity.this, ContactUsActivity.class);
-                startActivity(intent);
-            }
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent(FAQActivity.this, ContactUsActivity.class);
+            startActivity(intent);
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.TheSecondDawnImageButton12) {
+            finish();
+        }
     }
 }
 
