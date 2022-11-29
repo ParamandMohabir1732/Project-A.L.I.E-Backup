3450e5a1d8a43d2cf16f2378aa275597b490aaee
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-11-29 01:21:28 -0500

    Implemented Left Arrow Function Button to go back to Previous Screen in ContactUsActivity.java and also added appropriate comments to all java files.

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/ContactUsActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/ContactUsActivity.java
@@ -1,14 +1,49 @@
+/*
+Paramand Mohabir N01421732
+CENG322 Section 0NC
+Software Project
+*/
+/*
+Vladislav Vassilyev N01436627
+CENG322 Section 0NB
+Software Project
+*/
+/*
+Dave Patel N01465129
+CENG322 Section 0NA
+Software Project
+*/
+/*
+Paolo Brancato N01434080
+CENG322 Section ONC
+Software Project
+*/
+
 package ca.theseconddawn.it.a.l.i.e;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class ContactUsActivity extends AppCompatActivity {
+public class ContactUsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton leftArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        leftArrow = findViewById(R.id.TheSecondDawnImageButton13);
+        leftArrow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.TheSecondDawnImageButton13) {
+            finish();
+        }
     }
 }
