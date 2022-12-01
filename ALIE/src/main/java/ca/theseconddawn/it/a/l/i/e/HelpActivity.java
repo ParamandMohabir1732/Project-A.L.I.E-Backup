51eb85afee368dae4069449694632a282c908ecd
DavePatel5129 <Dave.p07@outlook.com>
2022-12-01 00:30:58 -0500

    implemented night mode functionality in HelpScreen

--- ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/HelpActivity.java
+++ ALIE/src/main/java/ca/theseconddawn/it/a/l/i/e/HelpActivity.java
@@ -22,19 +22,28 @@ Software Project
 package ca.theseconddawn.it.a.l.i.e;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class HelpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton leftArrow;
     private Button callUs;
     private ImageButton contactUs, aboutUs, faq;
+    SwitchCompat switchCompat;
+    SharedPreferences sharedPreferences = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,8 +85,37 @@ public class HelpActivity extends AppCompatActivity implements View.OnClickListe
         if (view.getId() == R.id.TheSecondDawnImageButton15) {
             finish();
         }
+
+        switchCompat = findViewById(R.id.nightmode_switch);
+        sharedPreferences = getSharedPreferences("night", 0);
+        Boolean booleanValue = sharedPreferences.getBoolean("night_mode", true);
+        if (booleanValue){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchCompat.setChecked(false);
+    }
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    switchCompat.setChecked(true);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",true);
+                    editor.apply();
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    switchCompat.setChecked(false);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("night_mode",false);
+                    editor.apply();
+
+                }
+            }
+        });
+
     }
 }
+
 
 
 
