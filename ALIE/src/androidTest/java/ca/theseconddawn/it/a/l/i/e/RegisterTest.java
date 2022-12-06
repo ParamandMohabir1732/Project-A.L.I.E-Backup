ff2c4150cea012de938ba60f31c0a6c55da7f0a2
ParamandMohabir1732 <kishenmohabir@gmail.com>
2022-12-05 21:07:44 -0500

    Added 1 new Espresso Case for Invalid Password and you need at least 1 capital alphabet letter.

--- ALIE/src/androidTest/java/ca/theseconddawn/it/a/l/i/e/RegisterTest.java
+++ ALIE/src/androidTest/java/ca/theseconddawn/it/a/l/i/e/RegisterTest.java
@@ -60,4 +60,13 @@ public class RegisterTest {
         onView(ViewMatchers.withId(R.id.TheSecondDawnEditText4)).perform(typeText("TheSecondDawn$"), closeSoftKeyboard());
         onView(ViewMatchers.withId(R.id.TheSecondDawnButton2)).perform(click());
     }
+
+    @Test
+    public void invalidPasswordAlphabet() {
+        onView(ViewMatchers.withId(R.id.TheSecondDawnEditText11)).perform(typeText("Titus"), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.TheSecondDawnEditText3)).perform(typeText("Titus@gmail.com"), closeSoftKeyboard());
+        //Password Needs at least 1 Upper case Alphabet
+        onView(ViewMatchers.withId(R.id.TheSecondDawnEditText4)).perform(typeText("1111111$"), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.TheSecondDawnButton2)).perform(click());
+    }
 }
